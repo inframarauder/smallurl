18903e90ecaa70ac90faf60eacd6c467ea9af4e8
subhasis020299 <subhasisdas125@gmail.com>
2020-10-05 21:48:29 +0530

    added axios config

---
+++ client/src/Configs/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = axios.create();
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = token;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
