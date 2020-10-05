1f7e9745abbffb913cab834f53f35d084a7af7a0
subhasis020299 <subhasisdas125@gmail.com>
2020-10-05 18:12:58 +0530

    added middlewares to routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,33 @@
 const router = require('express').Router();
 const UrlController = require('../controllers/UrlController');
 const AuthController = require('../controllers/AuthController');
+const AuthMiddleware = require('../middlewares/AuthMiddleware');
+const ValidationMiddleware = require('../middlewares/ValidationMiddleware');
 
 //auth
-router.post('/auth/singup', AuthController.signup);
-router.post('/auth/login', AuthController.login);
+router.post(
+  '/auth/singup',
+  ValidationMiddleware.validateUser,
+  AuthController.signup
+);
+router.post(
+  '/auth/login',
+  ValidationMiddleware.validateUser,
+  AuthController.login
+);
 
 //url
-router.post('/url/create', UrlController.createShortUrl);
-router.get('/url/dashboard', UrlController.getDashboard);
+router.post(
+  '/url/create',
+  AuthMiddleware.isAuthenticated,
+  ValidationMiddleware.validateUrl,
+  UrlController.createShortUrl
+);
+router.get(
+  '/url/dashboard',
+  AuthMiddleware.isAuthenticated,
+  UrlController.getDashboard
+);
 router.get('/url/search', UrlController.getUrl);
 router.delete('/url/delete/:urlId', UrlController.deleteUrl);
 router.get('/url/redirect/:shortUrl', UrlController.redirectToUrl);
