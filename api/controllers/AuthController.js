363f5c885c527467213a23e622a6c3b0495f07b7
subhasis020299 <subhasisdas125@gmail.com>
2020-10-05 17:12:59 +0530

    added AuthController

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,40 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+
+exports.signup = async (req, res) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: 'This email is already registered!' });
+    } else {
+      user = await new User(req.body).save();
+      const token = user.createToken();
+      return res.status(201).json({ token, userId: user._id });
+    }
+  } catch (error) {
+    console.error('Error in signup', error);
+    return res.status(500).json({ error: 'Internal server error!' });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found!' });
+    } else {
+      let valid = await bcrypt.compare(req.body.password, user.password);
+      if (valid) {
+        const token = user.createToken();
+        return res.status(200).json({ token, userId: user._id });
+      } else {
+        return res.status(401).json({ error: 'Passwords do not match!' });
+      }
+    }
+  } catch (error) {
+    console.error('Error in login!', error);
+    return res.status(500).json({ error: 'Internal server error!' });
+  }
+};
