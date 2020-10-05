f6e376887bcf11fb7f9fc666c0c28ab8eeb9170f
subhasis020299 <subhasisdas125@gmail.com>
2020-10-05 17:32:18 +0530

    integrated routes and controllers

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
+const UrlController = require('../controllers/UrlController');
+const AuthController = require('../controllers/AuthController');
 
 //auth
-router.post('/auth/singup');
-router.post('/auth/login');
+router.post('/auth/singup', AuthController.signup);
+router.post('/auth/login', AuthController.login);
 
 //url
-router.post('/url/create');
-router.get('/url/dashboard');
-router.get('/url/search');
-router.delete('/url/delete/:urlId');
-router.get('/url/redirect/:shortUrl');
+router.post('/url/create', UrlController.createShortUrl);
+router.get('/url/dashboard', UrlController.getDashboard);
+router.get('/url/search', UrlController.getUrl);
+router.delete('/url/delete/:urlId', UrlController.deleteUrl);
+router.get('/url/redirect/:shortUrl', UrlController.redirectToUrl);
 
 module.exports = router;
