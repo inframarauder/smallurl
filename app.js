3e7f2d10b1f8dc2b518bd3a60041591b4520b868
subhasis020299 <subhasisdas125@gmail.com>
2020-10-05 21:34:17 +0530

    react app setup with npm scripts

--- app.js
+++ app.js
@@ -33,6 +33,15 @@ app.get('/', (req, res) => {
 
 app.use('/api', api);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 //configure port and run server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server running on port' + port));
